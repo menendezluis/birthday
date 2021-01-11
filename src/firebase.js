cfaeaebc4b9287e287b930b2db1a11526f5a6ca8
Luis Menendez <76136932+menendezluis@users.noreply.github.com>
2021-01-11 12:15:47 -0600

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvq4AJW3mJknkLEJIygpGUL9kt1QeWqa8",
+    authDomain: "birthdaylm.firebaseapp.com",
+    projectId: "birthdaylm",
+    storageBucket: "birthdaylm.appspot.com",
+    messagingSenderId: "372226604131",
+    appId: "1:372226604131:web:ae55b84bcb929cac574c56"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
